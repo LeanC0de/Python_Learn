be869334c326042ca3ec870206d831ea4ae75962
Lean <32766984+LeanC0de@users.noreply.github.com>
2025-02-12 03:29:45 -0500

    Twosum

---
+++ test.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
+
+A1 = [2, 3, 4, 5, 5, 6, 7, 8]
+A2 = 11
+
+A3 = Solution()
+print(A3.twoSum(A1, A2))
+
